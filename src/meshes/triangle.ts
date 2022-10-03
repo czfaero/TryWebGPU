cfaab728c41174501083b724495c3c2a4b479d85
Chen Zaifeng <79635397+czfaero@users.noreply.github.com>
2022-10-03 16:40:37 +0900

    Create triangle.ts

---
+++ src/meshes/triangle.ts
@@ -0,0 +1,19 @@
+
+export const GetVertex = function () {
+    const vertex = new Float32Array([
+        1,-1,1,
+        -1,-1,1,
+        0,1,1
+    ]);
+
+    return vertex;
+}
+export const GetIndices = function () {
+    const indices = new Uint32Array([
+        0, 1, 2
+    ]);
+
+    return indices;
+}
+
+
